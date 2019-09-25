9d9c511ea75c413ecfca3696eb3ef093efa83227
konakugur <konak.ugurr@gmail.com>
2019-09-25 17:49:06 +0300

    Photo views added.

---
+++ app/src/main/java/com/example/wallpaper/Utils/SquareImage.java
@@ -0,0 +1,30 @@
+package com.example.wallpaper.Utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class SquareImage extends ImageView {
+
+    public SquareImage(Context context) {
+        super(context);
+    }
+
+    public SquareImage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImage(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SquareImage(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(),getMeasuredWidth());
+    }
+}
